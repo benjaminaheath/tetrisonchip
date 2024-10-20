855d0480eb3991e441d45d609547ae9662c679f9
Ben Heath <benjaheath@gmail.com>
2024-10-20 22:10:43 +0100

    Implementation for piecegenerator

--- rtl/controller/piecegenerator.v
+++ rtl/controller/piecegenerator.v
@@ -1,16 +1,36 @@
-module piecegenerator #(
-    localparam num_pieces
-) (
+module piecegenerator (
     input enable, 
     input clk,
     input nreset,
-    output ready,
-    output [3*num_pieces:0] bits
+    output reg ready,
+    output reg [2:0] piece
 );
 
-lfsr bitgen (.clk(clk), .nreset(nreset), .bit(bit)); // LFSR bit generator
+    wire [2:0][14:0] bitsout;
+    lfsr bitgen0 (.clk(clk), .nreset(nreset), .out(bitsout[0]));
+    lfsr bitgen1 (.clk(clk), .nreset(nreset), .out(bitsout[1]));
+    lfsr bitgen2 (.clk(clk), .nreset(nreset), .out(bitsout[2]));
 
-always @(clk) begin
-end
+    wire [2:0] piecebits;
+    assign piecebits = {bitsout[0][0], bitsout[1][0], bitsout[2][0]};
+
+    reg [2:0] piececount;
+
+    always @(posedge clk or negedge nreset) begin
+        if(!nreset) begin
+            // reset state of piece generator
+            ready <= 0;
+            piece <= 3'b0;
+            piececount <= 3'b0;
+        end
+        else begin
+            // check the new generated piece is valid
+            if(piecebits != 3'b111) begin
+                
+            end
+
+        
+        end
+    end
 
 endmodule
